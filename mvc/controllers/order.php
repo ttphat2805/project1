8a4279e0e7b5173b690ffd14f40dd4b580298953
Trần Tấn Phát <phattran28052002@gmail.com>
2021-12-06 11:58:13 +0700

    Update order.php

--- mvc/controllers/order.php
+++ mvc/controllers/order.php
@@ -36,7 +36,6 @@ class order extends Controller {
         $phone = $_POST['phone'];
         $note = $_POST['note'];
         $total = $_POST['total'];
-        
         $data = [
             'memberid' => $_SESSION['user_infor']['user_id'],
             'name' => $name,
@@ -50,6 +49,7 @@ class order extends Controller {
             'coupon' => $_POST['coupon']
         ];
         $this->ordermethod->insertOder($data);
+        unset($_SESSION['cart_Item']);
         return   $this->view(
             "master2",
             [
